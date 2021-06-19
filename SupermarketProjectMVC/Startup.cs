f88d2fd78dc56b301ca437b29b0e4649ff231e0a
Stevenkhalife <Stevenkhalife@users.noreply.github.com>
2021-06-19 08:25:30 +0300

    google

--- SupermarketProjectMVC/Startup.cs
+++ SupermarketProjectMVC/Startup.cs
@@ -25,9 +25,13 @@ namespace SupermarketProjectMVC
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-         services.AddControllersWithViews();
-         services.AddRazorPages();
-         services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllersWithViews();
+            services.AddRazorPages();
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddAuthentication().AddGoogle(options => {
+                options.ClientId = Configuration["App:GoogleClientId"];
+                options.ClientSecret = Configuration["App:GoogleClientSecret"];
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
